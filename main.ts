3941ce7aab1e4bcd5561e898fc85ac4722927fbf
CoolKid0095 <81427682+CoolKid0095@users.noreply.github.com>
2021-04-02 08:34:35 -0600

    Update pxt.json, main.blocks, main.ts, images.g.jres, images.g.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,20 @@
 let myNecromancer = sprites.create(assets.image`Necromancer`, SpriteKind.Player)
-controller.moveSprite(myNecromancer)
+controller.moveSprite(myNecromancer, 50, 50)
 scene.cameraFollowSprite(myNecromancer)
+animation.runImageAnimation(
+myNecromancer,
+assets.animation`myNecromancerWalkAnimation`,
+300,
+true
+)
+let myKnight = sprites.create(assets.tile`myTile`, SpriteKind.Enemy)
+animation.runImageAnimation(
+myKnight,
+assets.animation`myKnightWalkAnimation`,
+300,
+true
+)
+color.setColor(3, color.rgb(150, 150, 150))
 forever(function () {
     tiles.setTilemap(tilemap`level1`)
 })
