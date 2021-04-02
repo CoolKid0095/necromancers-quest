0d79fe217b6560b998c7423e0d8394bb4a0055af
CoolKid0095 <81427682+CoolKid0095@users.noreply.github.com>
2021-04-02 08:50:17 -0600

    Update main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,23 @@
+function loadTilemap (numOfTileMap: number) {
+    if (numOfTileMap == 1) {
+        tiles.setTilemap(tilemap`level2`)
+    } else if (numOfTileMap == 2 || numOfTileMap == 3) {
+        tiles.setTilemap(tilemap`level3`)
+    } else if (numOfTileMap == 4) {
+        tiles.setTilemap(tilemap`level4`)
+    }
+}
+let mySprite2 = null
+let mySprite3 = null
+let O = 0
+let list2 = null
+let L = 0
+let M = 0
+let numOfColumns = 0
+let N = 0
+let numOfRows = 0
+let I = 0
+let J = 0
 let myNecromancer = sprites.create(assets.image`Necromancer`, SpriteKind.Player)
 controller.moveSprite(myNecromancer, 50, 50)
 scene.cameraFollowSprite(myNecromancer)
@@ -15,6 +35,12 @@ assets.animation`myKnightWalkAnimation`,
 true
 )
 color.setColor(3, color.rgb(150, 150, 150))
+let numOfRows2 = 0
+let numOfColmuns2 = 0
+loadTilemap(1)
+let following = 1
+let formation = 1
+let currentMap = 1
 forever(function () {
     tiles.setTilemap(tilemap`level1`)
 })
